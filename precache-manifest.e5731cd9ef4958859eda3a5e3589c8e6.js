9ddb0d6338ce6d1e6c8cdc2adf08ec4be88e3415
yuxiaochao <yuxiaochao@zhuge.com>
2019-04-16 22:34:07 +0800

    Updates

---
+++ precache-manifest.e5731cd9ef4958859eda3a5e3589c8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/rpages/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d2838563040d593ca9a8",
+    "url": "/rpages/static/js/runtime~main.95ff5101.js"
+  },
+  {
+    "revision": "1402f5ba1cb675ac03ca",
+    "url": "/rpages/static/js/main.5c9a64ab.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/rpages/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "1402f5ba1cb675ac03ca",
+    "url": "/rpages/static/css/main.f6a45af7.chunk.css"
+  },
+  {
+    "revision": "b519556eae014436289d3395d2b5b348",
+    "url": "/rpages/index.html"
+  }
+];
